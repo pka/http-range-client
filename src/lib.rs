37e04563ac2d7fcfae56fdeaf0445fc5ad3954e2
Pirmin Kalberer <pka@sourcepole.ch>
2021-11-11 08:46:45 +0100

    Add feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,10 @@ extern crate log;
 mod buffered_range_client;
 mod error;
 mod range_client;
+#[cfg(feature = "reqwest")]
 mod reqwest_client;
 
 pub use buffered_range_client::BufferedHttpRangeClient;
 pub use error::*;
+#[cfg(feature = "reqwest")]
 pub(crate) use reqwest_client::HttpClient;
